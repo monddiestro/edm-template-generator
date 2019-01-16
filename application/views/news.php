9234e7e6feb647049a83bdff9c5ab026a20fa2d4
Mond <reymond.diestro@carmudi.com.ph>
2019-01-16 18:46:23 +0800

    new template generator

---
+++ application/views/news.php
@@ -0,0 +1,144 @@
+<div class="container-fluid">
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card">
+            <div class="card-header">
+                <h5>News Template</h5>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="">Header Title</label>
+                            <input type="text" placeholder="Title" id="header_title" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Header Image</label>
+                            <input type="text" placeholder="Image URL" id="header_img" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Header Description</label>
+                            <textarea name="" id="header_desc" class="form-control" placeholder="Description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Header URL</label>
+                            <input type="text" placeholder="URL" id="header_url" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 1 Title</label>
+                           <input type="text" placeholder="Title" id="news_one_title" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 1 Image</label>
+                            <input type="text" placeholder="Image URL" id="news_one_img" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 1 Description</label>
+                            <textarea name="" id="news_one_desc" class="form-control" placeholder="Description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 1 URL</label>
+                            <input type="text" placeholder="URL" id="news_one_url" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 2 Title</label>
+                           <input type="text" placeholder="Title" id="news_two_title" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 2 Image</label>
+                            <input type="text" placeholder="Image URL" id="news_two_img" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 2 Description</label>
+                            <textarea name="" id="news_two_desc" class="form-control" placeholder="Description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 2 URL</label>
+                            <input type="text" placeholder="URL" id="news_two_url" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 3 Title</label>
+                           <input type="text" placeholder="Title" id="news_three_title" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 3 Description</label>
+                            <textarea name="" id="news_three_desc" class="form-control" placeholder="Description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">News 3 URL</label>
+                            <input type="text" placeholder="URL" id="news_three_url" class="form-control"/>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="">Features 1 Title</label>
+                           <input type="text" placeholder="Title" id="features_one_title" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Features 1 Image</label>
+                            <input type="text" placeholder="Image URL" id="features_one_img" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Features 1 Description</label>
+                            <textarea name="" id="features_one_desc" class="form-control" placeholder="Description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Features 1 URL</label>
+                            <input type="text" placeholder="URL" id="features_one_url" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Features 2 Title</label>
+                           <input type="text" placeholder="Title" id="features_two_title" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Features 2 Description</label>
+                            <textarea name="" id="features_two_desc" class="form-control" placeholder="Description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Features 2 URL</label>
+                            <input type="text" placeholder="URL" id="features_one_url" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Tips and Advice 1 Title</label>
+                           <input type="text" placeholder="Title" id="tips_one_title" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Tips and Advice 1 Image</label>
+                            <input type="text" placeholder="Image URL" id="tips_one_img" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Tips and Advice 1 Description</label>
+                            <textarea name="" id="tips_one_desc" class="form-control" placeholder="Description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Tips and Advice 1 URL</label>
+                            <input type="text" placeholder="URL" id="tips_one_url" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Tips and Advice 2 Title</label>
+                           <input type="text" placeholder="Title" id="tips_two_title" class="form-control"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Tips and Advice 2 Description</label>
+                            <textarea name="" id="tips_two_desc" class="form-control" placeholder="Description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="">Tips and Advice 2 URL</label>
+                            <input type="text" placeholder="URL" id="tips_two_url" class="form-control"/>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="form-group">
+                            <label for="">Generated Template</label>
+                            <textarea class="form-control" name="" id="" rows="60"><?php echo $template ?></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-primary" id="btn_generate">Generate</button>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
